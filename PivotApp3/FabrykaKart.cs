ff6012c869222a435c39d756ff249e3a7d8d8b1a
Grzegorz Marczewski <gelomarc@gmail.com>
2013-06-09 09:55:53 +0200

    pierdoly

---
+++ PivotApp3/FabrykaKart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PivotApp3
+{
+    public class FabrykaKart
+    {
+        
+        private static Karta instancja;
+        private FabrykaKart() 
+        {
+
+        }
+
+   public static Karta Instancja
+   {
+      get 
+      {
+         if (instancja == null)
+         {
+            instancja = new Karta();
+         }
+         return instancja;
+      }
+   }
+    }
+}
---
+++ PivotApp3/Sealed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PivotApp3
+{
+    public class Sealed
+    {
+        public Sealed()
+        {
+
+        }
+        public Sealed(String dodatek1, String dodatek2, String dodatek3, String dodatek4, String dodatek5, String dodatek6)
+        {
+            String[] dodatki = {dodatek1,dodatek2,dodatek3,dodatek4,dodatek5,dodatek6};
+           
+        }
+    }
+}
